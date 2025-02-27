0385c8f263cf1dbd6fb96236a631d598dabf19ea
farukerat <farukerat1327@gmail.com>
2025-02-27 11:47:24 +0300

    fix dependencies

--- src/main/java/technarts/mongock/Application.java
+++ src/main/java/technarts/mongock/Application.java
@@ -1,7 +1,25 @@
 package technarts.mongock;
 
+import com.github.fakemongo.Fongo;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 public class Application {
     public static void main(String[] args) {
+        Fongo fongo = new Fongo("mockDB");
 
+        MongoClient mongoClient = fongo.getMongo();
+
+        MongoDatabase database = mongoClient.getDatabase("testDB");
+
+        MongoCollection<Document> collection = database.getCollection("users");
+
+        Document doc = new Document("name", "Alice").append("age", 25);
+        collection.insertOne(doc);
+
+        Document found = collection.find(new Document("name", "Alice")).first();
+        System.out.println(found);
     }
 }
