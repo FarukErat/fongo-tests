3a644feea6dbbb9825aa75127265913effbd77b4
farukerat <farukerat1327@gmail.com>
2025-02-28 02:00:09 +0300

    write speed test

--- src/main/java/technarts/mongock/Application.java
+++ src/main/java/technarts/mongock/Application.java
@@ -7,19 +7,55 @@ import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
 public class Application {
+    private static long timeOperation(Runnable operation) {
+        long startTime = System.nanoTime();
+        operation.run();
+        return (System.nanoTime() - startTime) / 1_000_000; // Convert to milliseconds
+    }
+
+    private static void insertDocuments(MongoCollection<Document> collection, int numDocs) {
+        for (int i = 0; i < numDocs; i++) {
+            collection.insertOne(new Document("_id", i).append("value", "data_" + i));
+        }
+    }
+
+    private static void updateDocuments(MongoCollection<Document> collection, int numDocs) {
+        for (int i = 0; i < numDocs; i++) {
+            collection.updateOne(new Document("_id", i), new Document("$set", new Document("updated", true)));
+        }
+    }
+
+    private static void queryDocuments(MongoCollection<Document> collection, int numDocs) {
+        for (int i = 0; i < numDocs; i++) {
+            collection.find(new Document("_id", i)).first();
+        }
+    }
+
+    private static void deleteDocuments(MongoCollection<Document> collection, int numDocs) {
+        for (int i = 0; i < numDocs; i++) {
+            collection.deleteOne(new Document("_id", i));
+        }
+    }
+
     public static void main(String[] args) {
         Fongo fongo = new Fongo("mockDB");
-
         MongoClient mongoClient = fongo.getMongo();
-
         MongoDatabase database = mongoClient.getDatabase("testDB");
-
         MongoCollection<Document> collection = database.getCollection("users");
+        int numDocs = 10_000;
 
-        Document doc = new Document("name", "Alice").append("age", 25);
-        collection.insertOne(doc);
+        System.out.println("Testing with " + numDocs + " documents...");
 
-        Document found = collection.find(new Document("name", "Alice")).first();
-        System.out.println(found);
+        long insertTime = timeOperation(() -> insertDocuments(collection, numDocs));
+        System.out.println("Insert time: " + insertTime + " ms");
+
+        long updateTime = timeOperation(() -> updateDocuments(collection, numDocs));
+        System.out.println("Update time: " + updateTime + " ms");
+
+        long queryTime = timeOperation(() -> queryDocuments(collection, numDocs));
+        System.out.println("Query time: " + queryTime + " ms");
+
+        long deleteTime = timeOperation(() -> deleteDocuments(collection, numDocs));
+        System.out.println("Delete time: " + deleteTime + " ms");
     }
 }
